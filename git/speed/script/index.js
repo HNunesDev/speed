554386b238715ae9acdbca97b84e847ea3feca3b
HenriqueNunesGuilhem <103960952+HenriqueNunesGuilhem@users.noreply.github.com>
2022-08-18 16:07:13 -0300

    MH - 0004
    Adição de duas novas telas, caso login seja feito e tela de cadastramento

---
+++ git/speed/script/index.js
@@ -0,0 +1,40 @@
+console.log("Hello world")
+
+var teste = 1
+
+const cs = "teste"
+
+//var entradaClass = document.getElementsByClassName("testeClass")
+
+
+console.log(entrada)
+
+function funcaoum(){
+    var entrada = document.getElementById("inputSpeed").value
+    console.log(entrada)
+
+    var saida = document.getElementById("aparicao")
+
+
+    if ("1" != entrada) {
+        saida.innerHTML += `${entrada} \n
+    }else{
+        saida.innerHTML = ! Esse valor é um número 1" + entrada + "\n"
+        saida.innerHTML = `Esse valor é um numero 1 ${entrada}\n `
+    }
+}
+
+/**
+ * const arrow = ()=>{
+
+== igual
+=== mesmo valor e tipo
+!= diferente
+>maior
+<menr 
+<=menor igual
+>= maior igual
+%% e
+|| ou
+
+*/
